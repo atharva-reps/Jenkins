87f89017e0eec1390658c6e67a01107e3e77c0d9
Atharva Kale <atharvakale3011@gmail.com>
2025-09-16 13:50:58 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,38 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, element):
+        self.stack.append(element)
+        print(f"Pushed {element} into the stack")
+
+    def pop(self):
+        if not self.isempty():
+            element = self.stack.pop()
+            print(f"Popped {element} from the stack")
+            return element
+        else:
+            print(f"Stack is empty")
+            return None
+
+    def peek(self):
+        return len(self.stack)
+
+    def isempty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+    def display(self):
+        print("Stack elements: ", self.stack)
+
+stack = Stack()
+stack.push(10)
+stack.push(25)
+stack.push(53)
+stack.display()
+print("Top element is ", stack.peek())
+stack.pop()
+stack.display()
+print("Stack size is ", stack.size())
